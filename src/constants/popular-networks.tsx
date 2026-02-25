aeb6973cfad25ccf33789c2744a54e4b059afbd8
Anurag <129600042+OM2309@users.noreply.github.com>
2026-02-25 23:11:11 +0530

    Re-add Ethereum to popular networks list

--- src/constants/popular-networks.tsx
+++ src/constants/popular-networks.tsx
@@ -8,8 +8,8 @@ import {
 
 const popularNetworks = [
   { id: 'solana',   name: 'Solana',   icon: <SiSolana className="text-[#00FFA3] text-4xl" /> },   
-  { id: 'ethereum', name: 'Ethereum', icon: <SiEthereum className="text-[#627EEA] text-4xl" /> }, 
   { id: 'sui',      name: 'Sui',      icon: <SiSui className="text-[#4DA2FF] text-4xl" /> },      
+  { id: 'ethereum', name: 'Ethereum', icon: <SiEthereum className="text-[#627EEA] text-4xl" /> },
   { 
     id: 'bnb', 
     name: 'BNB Chain', 
